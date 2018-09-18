ad0ac7af98e56c07e44d7b8f27f7d980a068d5bc
patrickuysy <2015081400@ust-ics.mygbiz.com>
2018-09-18 15:10:19 +0800

    Initial commit

---
+++ app/src/main/java/sy/patrick/com/sypatrickexam1/SyPatrickExam1.java
@@ -0,0 +1,13 @@
+package sy.patrick.com.sypatrickexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SyPatrickExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sy_patrick_exam1);
+    }
+}
